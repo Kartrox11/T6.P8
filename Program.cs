2adb03ab90f22ed10c95e4311c6a0dd27d0f111c
Evgeniy Voronow <Evgelion11@gmail.com>
2023-01-29 20:27:14 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel;
+
+namespace Т6.П8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+
+            //1
+            //for (int i = 0; i <= 15; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //for (int j = 0; j <= 20; j++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+            //Console.ReadKey();
+
+            // 2
+            //for (int i = 0; i <= 15; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine("");
+            //for (int i = 0; i <= 20; i++)
+            //{
+            //    for (int j = 0; j <= 14; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("#");
+            //}
+            //Console.ReadKey();
+
+            // 3
+            //for (int j = 0; j <= 19; j++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+
+            //for (int i = 0; i <= 15; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.ReadKey();
+
+            // 4
+            //for (int i = 0; i <= 20; i++)
+            //{
+
+            //    for (int j = 0; j <= 14; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("#");
+            //}
+            //for (int i = 0; i <= 15; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.ReadKey();
+
+            //Задание 2
+
+            //for (int a = 0; a <= 15; a++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine();
+            //for (int b = 0; b <= 20; b++)
+            //{
+            //    Console.WriteLine("#              #");
+            //}
+            //for (int c = 0; c <= 15; c++)
+            //{
+            //    Console.Write("#");
+            //}
+
+            //Задание 3
+            //for (int j = 0; j <= 3; j++)
+            //{
+            //    Console.WriteLine("");
+            //    for (int i = 0; i <= 4; i++)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+
+
+            //Здание 4
+            //for (int i = 0; i <= 3; i++)
+            //{
+            //    for (int j = 0; j <= 3; j++)
+            //    {
+            //        Console.Write("2");
+            //        for (int k = 0; k <= 1; k++)
+            //        {
+            //            Console.Write(" ");
+            //        }
+
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+        }
+    }
+}
